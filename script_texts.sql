16b330d5ad4fe2beb4b497b3de3489ad76304151
Blizz <stylar91@google.com>
2012-04-20 13:22:45 +0200

    Add script_texts.sql

---
+++ script_texts.sql
@@ -0,0 +1,60 @@
+################################################################################
+#####                                                                      #####
+#####          GGGGGGGG        AA         DDDDDDD    BBBBBBB               #####
+#####         GG              A  A        D     DD   B      B              #####
+#####        GG    GGG       A    A       D      D   BBBBBBB               #####
+#####         GG     GG     AAAAAAAA      D     DD   B      B              #####
+#####          GGGGGGG     A        A     DDDDDDD    BBBBBBB               #####
+#####                             CREW                                     #####
+################################################################################
+# Copyright (C) 2007-2012 GMDB <http://sourceforge.net/projects/gm-db>         #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+SET NAMES 'utf8';
+
+##### script_texts #####
+
+UPDATE script_texts SET content_loc3=NULL;
+
+UPDATE script_texts SET content_loc3='HILFE!' WHERE entry=-1000009;
+UPDATE script_texts SET content_loc3='Lass uns gehen.' WHERE entry=-1000140;
+UPDATE script_texts SET content_loc3='Loken?! Das ist doch einfach lästig... Wir haben vielleicht die Eisenzwerge ausgeschaltet, doch ich möchte wetten, dass es irgendwo noch eine weitere Maschine gibt, die am laufenden Band massenweise Eisenvrykul ausspuckt.' WHERE entry=-1599066;
+UPDATE script_texts SET content_loc3='Wie?' WHERE entry=-1595074;
+UPDATE script_texts SET content_loc3='Natürlich.' WHERE entry=-1576011;
+UPDATE script_texts SET content_loc3='Dem König zum Gruße, Baby!' WHERE entry=-1230002;
+UPDATE script_texts SET content_loc3='Findet Gryan Starkmantel' WHERE entry=-1060004;
+UPDATE script_texts SET content_loc3='Findet Gryan Starkmantel' WHERE entry=-1060003;
+UPDATE script_texts SET content_loc3='Findet Gryan Starkmantel' WHERE entry=-1060002;
+UPDATE script_texts SET content_loc3='Findet Gryan Starkmantel' WHERE entry=-1060001;
+UPDATE script_texts SET content_loc3='Findet Gryan Starkmantel' WHERE entry=-1060000;
+UPDATE script_texts SET content_loc3='Findet Gryan Starkmantel' WHERE entry=-1060005;
+UPDATE script_texts SET content_loc3='Pass auf!' WHERE entry=-1000482;
+UPDATE script_texts SET content_loc3='Hilfe!' WHERE entry=-1000105;
+UPDATE script_texts SET content_loc3='Was ist das denn?' WHERE entry=-1609255;
+UPDATE script_texts SET content_loc3='Lasst es mich wissen, wenn Ihr bereit seid. Früher wäre besser als später... von wegen langsames Sterben durch Gift und so weiter.' WHERE entry=-1800042;
+UPDATE script_texts SET content_loc3='Der Klinge, die mein Leben beendete, so nahe zu sein... Die Schmerzen... erneuert.' WHERE entry=-1668022;
+UPDATE script_texts SET content_loc3='Kommt, $N. Fürst Sturmgrimm erwartet Euch.' WHERE entry=-1000606;
+UPDATE script_texts SET content_loc3='Fürst Illidan wird hier bald eintreffen.' WHERE entry=-1000607;
+UPDATE script_texts SET content_loc3='Aber... Mein Fürst, ich verstehe nicht. $N... Er ist der Ork, der...' WHERE entry=-1000609;
+UPDATE script_texts SET content_loc3='Es wird vollbracht, mein Fürst.' WHERE entry=-1000610;
+UPDATE script_texts SET content_loc3='Ihr dachtet also Ihr könntet Mor\'ghor zum Narren halten, wie? Bevor Ihr Fürst Illidan übergeben werdet, werdet Ihr Schmerzen fühlen, deren Existenz Euch bisher unbekannt war. Mit Freuden werde ich meine Rache auskosten.' WHERE entry=-1000611;
+UPDATE script_texts SET content_loc3='Krieger des Drachenmals, schart euch zusammen! Einer unter euch hat den Rang des Hochlords erreicht! Verneigt euch aus Ehrfurcht! Zeigt eure Hochachtung und Loyalität gegenüber Hochlord $N!' WHERE entry=-1000612;
+UPDATE script_texts SET content_loc3='Es lebe Fürst Illidan!' WHERE entry=-1000613;
+UPDATE script_texts SET content_loc3='Seid gegrüßt, $N.' WHERE entry=-1000022;
+UPDATE script_texts SET content_loc3='Hmmm...' WHERE entry=-1000028;
+UPDATE script_texts SET content_loc3='Auf geht\'s!' WHERE entry=-1638009;
+UPDATE script_texts SET content_loc3='Hilfe!' WHERE entry=-1638014;
+
